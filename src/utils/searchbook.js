c6c8157c455ebf3c76df8683c7e9db5813dd2e38
Maulika <maulika26@gmail.com>
2022-04-16 17:16:46 -0400

    api error

--- src/utils/searchbook.js
+++ src/utils/searchbook.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const handleSearch = async ({text, setIsLoading, setMatches}) => {
     try {
         setIsLoading(true);
-        const response = await axios.get(`https://openlibrary.org/search.json?q=${text}`);
+        const response = await axios.get(`http://openlibrary.org/search.json?q=${text}`);
         const matches = response.data.docs.filter((book) => {
             const regex = new RegExp(`${text}`, "gi");
             return book.title.match(regex);
